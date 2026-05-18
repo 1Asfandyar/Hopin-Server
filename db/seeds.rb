f9eb863b81c7917ff2434f3daf696f0eb3d6b93e
Abdur Rahman <khalil.ahmad1547@gmail.com>
2026-05-18 15:39:26 +0500

    feat: add account seeding functionality to populate default accounts for test users

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,4 @@ require_relative 'seed/currency_seed'
 Rails.logger.info "Seeded currencies. Total count: #{Currency.count}"
 Rails.logger.info "Seeding categories..."
 require_relative 'seed/category_seed'
+require_relative 'seed/account_seed'
